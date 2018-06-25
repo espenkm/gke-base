db31a211d2683eef237499d042ba5e9df6b8f5b7
espenkm <espenkm@gmail.com>
2018-06-25 13:59:19 +0200

    backup

--- app/app.js
+++ app/app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello crule World!');
 });
 app.listen(3000, function () {
-  console.log('Example app listening on port df 3000!');
+  console.log('Example app listening on port 3000!');
 });
 
